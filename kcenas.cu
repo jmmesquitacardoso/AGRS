7fc56672d8c7c7febb1a6847985c0d729462b8a4
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2015-12-15 00:17:13 +0000

    refactor and code cleaning

--- kcenas.cu
+++ kcenas.cu
@@ -4,23 +4,13 @@
 #include <ctime>
 #include <cmath>
 #include <cfloat>
-#include <string>
 #include <vector>
-#include <algorithm>
-#include <iterator>
-#include <iomanip>
-#include <thrust/reduce.h>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
-#include <thrust/sort.h>
-#include <thrust/binary_search.h>
-#include <thrust/fill.h>
-#include <thrust/transform.h>
 #include <cuda.h>
 #include <curand_kernel.h>
 
 using namespace std;
-#define PI 3.14159265359
 #define MAX_NUMBER_OF_ITERATIONS 20
 
 #define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
@@ -37,13 +27,13 @@ __global__ void setup_kernel(curandState *state) {
     curand_init(7+id, id, 0, &state[id]);
 }
 
-__global__ void generate_normal_kernel(curandState *state, float *data_x, float *data_y) {
+__global__ void generate_normal_kernel(curandState *state, float *xPoints, float *yPoints) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   /* Copy state to local memory for efficiency */
   curandState localState = state[i];
   /* Generate pseudo-random uniforms */
-  data_x[i] = curand_normal(&localState);
-  data_y[i] = curand_normal(&localState);
+  xPoints[i] = curand_normal(&localState);
+  yPoints[i] = curand_normal(&localState);
   /* Copy state back to global memory */
   state[i] = localState;
 }
@@ -54,13 +44,13 @@ float compute_distance(float x1,float x2,float y1,float y2) {
 }
 
 __global__
-void mapFunction(int * map_data_cluster_index, float *data_x, float *cluster_x, float *data_y, float *cluster_y, int numberOfClusters) {
+void mapFunction(int * map_data_cluster_index, float *xPoints, float *xCentroids, float *yPoints, float *yCentroids, int numberOfClusters) {
   int j = threadIdx.x + blockIdx.x * blockDim.x;
   int index = 0;
   float minDistance = FLT_MAX;
 
   for(int i = 0; i < numberOfClusters; i++) {
-    float currentDistance = compute_distance(data_x[j],cluster_x[i],data_y[j],cluster_y[i]);
+    float currentDistance = compute_distance(xPoints[j],xCentroids[i],yPoints[j],yCentroids[i]);
     if(currentDistance<minDistance)
     {
       minDistance = currentDistance;
@@ -71,15 +61,15 @@ void mapFunction(int * map_data_cluster_index, float *data_x, float *cluster_x, 
 }
 
 __global__
-void reduce (int *data_cluster_index, float *data_x, float *data_y, float *centroids_x, float *centroids_y, float *sumX, float *sumY, int *nElemsX, int *nElemsY) {
+void reduce (int *clusterIndex, float *xPoints, float *yPoints, float *sumX, float *sumY, int *nElemsX, int *nElemsY) {
   int j = threadIdx.x + blockIdx.x * blockDim.x;
 
-  int clusterIndex = data_cluster_index[j];
+  int index = clusterIndex[j];
 
-  atomicAdd(&sumX[clusterIndex],data_x[j]);
-  atomicAdd(&sumY[clusterIndex],data_y[j]);
-  atomicAdd(&nElemsX[clusterIndex],1);
-  atomicAdd(&nElemsY[clusterIndex],1);
+  atomicAdd(&sumX[index],xPoints[j]);
+  atomicAdd(&sumY[index],yPoints[j]);
+  atomicAdd(&nElemsX[index],1);
+  atomicAdd(&nElemsY[index],1);
 }
 
 int main() {
@@ -103,29 +93,29 @@ int main() {
   setup_kernel<<<n, 128>>>(devStates);
   setup_kernel<<<numberOfClusters, 1>>>(devStates2);
 
-  thrust::host_vector<int> data_cluster_index(numberOfPoints);
+  thrust::host_vector<int> clusterIndex(numberOfPoints);
 
   //initialize all the points to belong in sentinel cluster -1
-  for (int i = 0; i < data_cluster_index.size(); i++) {
-    data_cluster_index[i]=-1;
+  for (int i = 0; i < clusterIndex.size(); i++) {
+    clusterIndex[i] = -1;
   }
 
   //creating and populating device vectors
-  thrust::device_vector<float> d_data_x(numberOfPoints);
-  thrust::device_vector<float> d_data_y(numberOfPoints);
-  thrust::device_vector<float> d_centroids_x(numberOfClusters);
-  thrust::device_vector<float> d_centroids_y(numberOfClusters);
-  thrust::device_vector<int> prev_index(numberOfPoints);
-  thrust::device_vector<int> d_data_cluster_index = data_cluster_index;
+  thrust::device_vector<float> xPoints(numberOfPoints);
+  thrust::device_vector<float> yPoints(numberOfPoints);
+  thrust::device_vector<float> xCentroids(numberOfClusters);
+  thrust::device_vector<float> yCentroids(numberOfClusters);
+  thrust::device_vector<int> previousIndex(numberOfPoints);
+  thrust::device_vector<int> deviceClusterIndex = clusterIndex;
 
-  int * data_cluster_index_ptr = thrust::raw_pointer_cast(&d_data_cluster_index[0]);
-  float *map_cluster_x = thrust::raw_pointer_cast(&d_centroids_x[0]);
-  float *map_data_x = thrust::raw_pointer_cast(&d_data_x[0]);
-  float *map_cluster_y = thrust::raw_pointer_cast(&d_centroids_y[0]);
-  float *map_data_y = thrust::raw_pointer_cast(&d_data_y[0]);
+  int *clusterIndexPointer = thrust::raw_pointer_cast(&deviceClusterIndex[0]);
+  float *xCentroidsPointer = thrust::raw_pointer_cast(&xCentroids[0]);
+  float *xPointsPointer = thrust::raw_pointer_cast(&xPoints[0]);
+  float *yCentroidsPointer = thrust::raw_pointer_cast(&yCentroids[0]);
+  float *yPointsPointer = thrust::raw_pointer_cast(&yPoints[0]);
 
-  generate_normal_kernel<<<n, 128>>>(devStates, map_data_x, map_data_y);
-  generate_normal_kernel<<<numberOfClusters, 1>>>(devStates2, map_cluster_x, map_cluster_y);
+  generate_normal_kernel<<<n, 128>>>(devStates, xPointsPointer, yPointsPointer);
+  generate_normal_kernel<<<numberOfClusters, 1>>>(devStates2, xCentroidsPointer, yCentroidsPointer);
 
   bool done = false;
   int i = 0;
@@ -147,16 +137,16 @@ int main() {
     CUDA_CALL(cudaMemset(nElemsX, 0, numberOfClusters *  sizeof(int)));
 
     CUDA_CALL(cudaMalloc((void **)&nElemsY, numberOfClusters * sizeof(int)));
-    CUDA_CALL(cudaMemset(nElemsX, 0, numberOfClusters *  sizeof(int)));
+    CUDA_CALL(cudaMemset(nElemsY, 0, numberOfClusters *  sizeof(int)));
 
     hostNElemsX = (int *)calloc(numberOfClusters, sizeof(int));
     hostNElemsY = (int *)calloc(numberOfClusters, sizeof(int));
 
     printf("Calling the map function with iteration number %d\n", i);
 
-    mapFunction<<<n, 128>>>(data_cluster_index_ptr,map_data_x,map_cluster_x,map_data_y,map_cluster_y, numberOfClusters);
+    mapFunction<<<n, 128>>>(clusterIndexPointer,xPointsPointer,xCentroidsPointer,yPointsPointer,yCentroidsPointer, numberOfClusters);
     // Check if the corresponding cluster for each point changed
-    done = thrust::equal(d_data_cluster_index.begin(),d_data_cluster_index.end(),prev_index.begin());
+    done = thrust::equal(deviceClusterIndex.begin(),deviceClusterIndex.end(),previousIndex.begin());
     if (done) {
       printf("Clusters for each point remained the same! Terminating...\n");
       break;
@@ -164,9 +154,8 @@ int main() {
       printf("Some points changed their corresponding cluster! Will do another iteration!\n");
     }
     // Copy this cluster index to another value to compare the next index to it
-    thrust::copy(d_data_cluster_index.begin(),d_data_cluster_index.end(),prev_index.begin());
-    //reduceFunction(d_data_cluster_index,d_data_x,d_data_y,d_centroids_x,d_centroids_y,numberOfPoints);
-    reduce<<<n, 128>>>(data_cluster_index_ptr, map_data_x, map_data_y, map_cluster_x, map_cluster_y, sumX, sumY, nElemsX, nElemsY);
+    thrust::copy(deviceClusterIndex.begin(),deviceClusterIndex.end(),previousIndex.begin());
+    reduce<<<n, 128>>>(clusterIndexPointer, xPointsPointer, yPointsPointer, sumX, sumY, nElemsX, nElemsY);
 
     CUDA_CALL(cudaMemcpy(hostSumX, sumX, numberOfClusters * sizeof(float), cudaMemcpyDeviceToHost));
     CUDA_CALL(cudaMemcpy(hostSumY, sumY, numberOfClusters * sizeof(float), cudaMemcpyDeviceToHost));
@@ -174,18 +163,18 @@ int main() {
     CUDA_CALL(cudaMemcpy(hostNElemsY, nElemsY, numberOfClusters * sizeof(int), cudaMemcpyDeviceToHost));
 
     for (int j = 0; j < numberOfClusters; j++) {
-      d_centroids_x[j] = (float) (hostSumX[j] / hostNElemsX[j]);
-      d_centroids_y[j] = (float) (hostSumY[j] / hostNElemsY[j]);
+      xCentroids[j] = (float) (hostSumX[j] / hostNElemsX[j]);
+      yCentroids[j] = (float) (hostSumY[j] / hostNElemsY[j]);
       printf("Number of points in cluster %d is %d\n",j,hostNElemsX[j]);
     }
 
     i++;
   }
 
-  for(int i = 0; i < d_centroids_x.size(); i++)
+  for(int i = 0; i < xCentroids.size(); i++)
   {
-    cout << "The X axis value of the centroid number " << i << " is " << d_centroids_x[i] << endl;
-    cout << "The Y axis value of the centroid number " << i << " is " << d_centroids_y[i] << endl;
+    cout << "The X axis value of the centroid number " << i << " is " << xCentroids[i] << endl;
+    cout << "The Y axis value of the centroid number " << i << " is " << yCentroids[i] << endl;
   }
 
   printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
